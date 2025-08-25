7cc2812ee8dfe268f61e23d2cd217d4276748767
Lou Kratz <219901029+loukratz-bv@users.noreply.github.com>
2025-08-25 18:38:14 -0400

    try to fix font path

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: process.env.NODE_ENV === 'production' ? '../qtype/interpreter/ui' : '.next',
   basePath: '/ui',
-  assetPrefix: process.env.NODE_ENV === 'production' ? "https://REWRITEME" : undefined,
+  assetPrefix: process.env.NODE_ENV === 'production' ? "https://rewriteme" : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true
