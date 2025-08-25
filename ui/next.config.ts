eaaa5e902f352d9215a142988a699085612909d6
Lou Kratz <219901029+loukratz-bv@users.noreply.github.com>
2025-08-25 14:57:12 -0400

    Try re-writing the ui path

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: process.env.NODE_ENV === 'production' ? '../qtype/interpreter/ui' : '.next',
   basePath: '/ui',
+  assetPrefix: process.env.NODE_ENV === 'production' ? "https://REWRITEME" : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true
